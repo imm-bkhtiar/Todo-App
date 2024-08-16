525db69954f224333618d2f13d6e8b38d05d1a9a
bkhtiar <bakhtiarimam801@gamil.com>
2024-08-16 15:12:55 +0700

    menyelesaikan todoapp

--- main.js
+++ main.js
@@ -1,17 +1,39 @@
 const btnTambah = document.querySelector(".container > .head > form button ");
 let todoInput = document.querySelector(".container > .head > form input");
-let i = 0;
-let lastKey = localStorage.getItem("lastKey");
+const listContainer = document.querySelector(".container > .todo");
+let i = localStorage.length > 0 ? localStorage.length : 0;
 
 btnTambah.addEventListener("click", (e) => {
-  const listContainer = document.querySelector(".container > .todo");
-  if (lastKey) {
-    i = parseInt(lastKey);
-  }
-
+  const listItem = `
+ <div class="list" key="${i}">
+  <p>${todoInput.value}</p>
+  <div class="action">
+   <a href="#">Selesai</a>
+   <a href="#">Hapus</a>
+  </div>
+ </div>`;
+  listContainer.innerHTML += listItem;
   localStorage.setItem(`${i++}`, todoInput.value);
-  localStorage.setItem("lastKey", `${i}`);
-
   e.preventDefault();
   todoInput.value = "";
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const storage = Object.entries(localStorage);
+  storage.sort((a, b) => {
+    return parseInt(a[0]) - parseInt(b[0]);
+  });
+
+  storage.forEach(([i, value]) => {
+    const listItem = `
+     <div class="list" key="${i}">
+       <p>${value}</p>
+       <div class="action">
+         <a href="#">Selesai</a>
+         <a href="#">Hapus</a>
+       </div>
+     </div>`;
+
+    listContainer.innerHTML += listItem;
+  });
 });
