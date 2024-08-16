2055cb67b820b9dabfb0488f1964faf9511193fc
bkhtiar <bakhtiarimam801@gamil.com>
2024-08-16 21:32:28 +0700

    menyelesaikan fitur delete dan ceklis pada list

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# BkhTodolist
+# Todo-App
+
+Todo-App adalah aplikasi to-do list berbasis web yang sederhana. Proyek ini dibuat untuk berlatih manipulasi DOM dengan JavaScript.
+
+## Fitur
+
+- Menambahkan tugas baru.
+- Menghapus tugas yang ada.
+- Menandai tugas sebagai selesai.
+
+## Teknologi yang Digunakan
+
+- HTML
+- CSS
+- JavaScript
--- main.js
+++ main.js
@@ -1,39 +1,99 @@
 const btnTambah = document.querySelector(".container > .head > form button ");
-let todoInput = document.querySelector(".container > .head > form input");
+const todoInput = document.querySelector(".container > .head > form input");
 const listContainer = document.querySelector(".container > .todo");
-let i = localStorage.length > 0 ? localStorage.length : 0;
+
+const getTodo = () => {
+  let todos = localStorage.getItem("todos")
+    ? JSON.parse(localStorage.getItem("todos"))
+    : [];
+  return todos;
+};
 
 btnTambah.addEventListener("click", (e) => {
+  const todos = getTodo();
+  const createTodo = {
+    id: Date.now(),
+    text: todoInput.value,
+    selesai: false,
+  };
+
   const listItem = `
- <div class="list" key="${i}">
-  <p>${todoInput.value}</p>
-  <div class="action">
-   <a href="#">Selesai</a>
-   <a href="#">Hapus</a>
-  </div>
- </div>`;
+  <div class="list" key="${createTodo.id}">
+    <p>${createTodo.text}</p>
+    <div class="action">
+      <a href="#">Hapus</a>
+      <a href="#">Selesai</a>
+    </div>
+  </div>`;
+
   listContainer.innerHTML += listItem;
-  localStorage.setItem(`${i++}`, todoInput.value);
+
+  todos.push(createTodo);
+  localStorage.setItem(`todos`, JSON.stringify(todos));
   e.preventDefault();
   todoInput.value = "";
+  loadbtn();
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-  const storage = Object.entries(localStorage);
-  storage.sort((a, b) => {
-    return parseInt(a[0]) - parseInt(b[0]);
+  const todos = getTodo();
+  todos.sort((a, b) => {
+    return a.id - b.id;
   });
 
-  storage.forEach(([i, value]) => {
+  todos.forEach((todo) => {
     const listItem = `
-     <div class="list" key="${i}">
-       <p>${value}</p>
+     <div class="list ${todo.selesai}" key="${todo.id}">
+       <p>${todo.text}</p>
        <div class="action">
-         <a href="#">Selesai</a>
          <a href="#">Hapus</a>
+         <a href="#">Selesai</a>
        </div>
      </div>`;
 
     listContainer.innerHTML += listItem;
   });
+  loadbtn();
 });
+
+const loadbtn = () => {
+  const todos = getTodo();
+  let lists = listContainer.querySelectorAll(".list");
+
+  lists.forEach((list) => {
+    const key = list.getAttribute("key");
+    const btnDelete = list.querySelector(".action a:nth-child(1)");
+    const btnSelesai = list.querySelector(".action a:nth-child(2)");
+    list.classList.remove("false");
+
+    // Funtion di bawah untuk delete list
+    btnDelete.addEventListener("click", () => {
+      const todos = getTodo();
+      const itemTidakRemove = todos.filter((todo) => `${todo.id}` !== key);
+      localStorage.setItem("todos", JSON.stringify(itemTidakRemove));
+      list.remove();
+    });
+
+    //function di bawah untuk centang list
+    btnSelesai.addEventListener("click", () => {
+      const todos = getTodo();
+      itemTidakTerseleksi = todos.filter((todo) => todo.id !== parseInt(key));
+      const itemSelesai = todos
+        .filter((todo) => `${todo.id}` === key)
+        .map((todo) => {
+          if (todo.id === parseInt(key)) {
+            todo.selesai = true;
+          } else {
+            todo.selesai = false;
+          }
+          return todo;
+        });
+
+      console.log(itemSelesai);
+
+      list.classList.toggle("true");
+      itemTidakTerseleksi.push(itemSelesai[0]);
+      localStorage.setItem("todos", JSON.stringify(itemTidakTerseleksi));
+    });
+  });
+};
